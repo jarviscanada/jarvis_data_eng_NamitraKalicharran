7c6b3307124d12a38ad5b196c650c048c096932e
NamitraKali <knamitra@gmail.com>
2021-09-24 06:21:59 +0000

    finished psql_docker.sh

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -44,8 +44,8 @@ case $cmd in
         exit $? 
     ;;
 
-    # In case someone types in neither create|start|stop
-    *)
+
+   *)
         echo "Illegal command"
 	echo 'Commands: start|stop|create'
 	exit 1
