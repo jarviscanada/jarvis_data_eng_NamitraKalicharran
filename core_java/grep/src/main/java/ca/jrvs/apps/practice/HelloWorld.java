7a0478afc230c6018474c9c155511b35a784f2a6
NamitraKali <knamitra@gmail.com>
2021-10-12 11:35:44 -0400

    tested maven

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
@@ -0,0 +1,8 @@
+package ca.jrvs.apps.practice;
+
+class HelloWorld {
+    // Prints "Hello World!" to the terminal window window
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    } 
+}
