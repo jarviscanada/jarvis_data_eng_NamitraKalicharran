a29e3947519e7946328a680de44c26ccff08c5dd
NamitraKali <knamitra@gmail.com>
2021-10-25 10:08:35 -0400

    Removed Generated files

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package ca.jrvs.apps.practice;
-
-class HelloWorld {
-    // Prints "Hello World!" to the terminal window window
-    public static void main(String args[]) {
-        System.out.println("Hello World!");
-    } 
-}
