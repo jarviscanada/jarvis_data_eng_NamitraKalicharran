77a6b35db93818f2b32966bb91375622903ed68a
NamitraKali <knamitra@gmail.com>
2021-10-28 10:55:03 -0400

    DAO skeleton implemented

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterHttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterHttpHelper.java
@@ -1,5 +1,35 @@
 package ca.jrvs.apps.twitter.dao;
 
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+
 public class TwitterHttpHelper implements HttpHelper {
-    
+    /**
+     * Dependencies are specified as private member variables
+     */
+    private OAuthConsumer consumer;
+    private HttpClient httpClient;
+
+    /**
+     * Constructor
+     * Setup dependencies using secrets
+     * 
+     * @param consumerKey
+     * @param consumerSecret
+     * @param accessToken
+     * @param tokenSecret
+     */
+    public TwitterHttpHelper(String consumerKey, String consumerSecret,
+        String accessToken, String tokenSecret) {
+        
+        consumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
+        consumer.setTokenWithSecret(accessToken, tokenSecret);
+
+        /**
+         * Default = single connection.
+         */
+        httpClient = new DefaultHttpClient();
+    }
 }
