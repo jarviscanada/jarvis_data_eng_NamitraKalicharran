6395ee538c8d9f675a3386641329b490d4dc6393
NamitraKali <knamitra@gmail.com>
2021-10-28 14:13:04 -0400

    Finished Helper

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/HttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/HttpHelper.java
@@ -1,5 +1,28 @@
 package ca.jrvs.apps.twitter.dao;
 
-interface HttpHelper {
+import java.net.URI;
+import org.apache.http.HttpResponse;
 
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+
+public interface HttpHelper {
+
+  /**
+   * Execute a HTTP Post call
+   * @param uri
+   * @return
+   * @throws OAuthCommunicationException
+   * @throws OAuthExpectationFailedException
+   * @throws OAuthMessageSignerException
+   */
+  HttpResponse httpPost(URI uri) throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException;
+
+  /**
+   * Execute a HTTP Get call
+   * @param uri
+   * @return
+   */
+  HttpResponse httpGet(URI uri);
 }
