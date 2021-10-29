489653456e9bf12287c1490875705e5bfae6cbe6
NamitraKali <knamitra@gmail.com>
2021-10-29 19:56:21 -0400

    finished compareMaps

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/compareMaps.java
@@ -0,0 +1,9 @@
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.Map;
+
+public class compareMaps {
+    public <K, V> boolean solution(Map<K, V> m1, Map<K, V> m2) {
+        return m1.equals(m2);
+    }
+}
